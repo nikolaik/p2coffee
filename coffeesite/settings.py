d7a1708939f6b52d3195922cf5a529ef7b32ed83
Nikolai R Kristiansen <nikolaik@gmail.com>
2016-05-21 14:57:36 +0200

    deployfixes

--- coffeesite/settings.py
+++ coffeesite/settings.py
@@ -103,9 +103,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Huey worker
 rconn = urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost:6379'))
