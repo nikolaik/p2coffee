b295e8c4573384060b3458512f1390f2b6ecfa75
Nikolai R Kristiansen <nikolaik@gmail.com>
2016-05-21 15:02:49 +0200

    Connect to postgres

--- coffeesite/settings.py
+++ coffeesite/settings.py
@@ -1,12 +1,11 @@
 """ https://docs.djangoproject.com/en/1.9/ref/settings/ """
 
+import dj_database_url
+from huey import RedisHuey
 import os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from urllib.parse import urlparse
 
-from huey import RedisHuey
-
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -79,7 +78,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
