bb55b48e4a5811148a59fa88138c41b20b856cdf
Nikolai Kristiansen <nikolai@lucida.no>
2017-08-17 14:51:40 +0200

    chore: Read SECRET_KEY from env

--- coffeesite/settings.py
+++ coffeesite/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0lw6f3ql#p38p2tt-9=j^!z^95bck35v@cldm+utdq2m&^0@z#'
+SECRET_KEY = os.getenv('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
