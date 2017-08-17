7903be51a28d9d457e99d341dc9bc3b72972f550
Nikolai Kristiansen <nikolai@lucida.no>
2017-08-17 15:14:31 +0200

    chore: read ALLOWED_HOSTS from env

--- coffeesite/settings.py
+++ coffeesite/settings.py
@@ -18,8 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 INSTALLED_APPS = [
